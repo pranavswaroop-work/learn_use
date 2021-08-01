821bff83c09065a8891d046a8c83b6e70634786d
pranavworks <pranavswaroop.work@outlook.com>
2021-08-01 22:00:48 +0530

    rt

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='Python/driver/chromedriver')
+driver = webdriver.Chrome(executable_path='Python/driver/chromedriver.exe')
 
 # Navigate to page
 driver.get('https://google.com')
